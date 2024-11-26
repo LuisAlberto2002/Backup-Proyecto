687e80e8c745075f759ac1294ca1d0091a80ba32
= <=>
2024-11-26 00:35:07 -0600

    Correcion del archivo rutas

--- AplicacionBanco/scr/rutas/rutas.js
+++ AplicacionBanco/scr/rutas/rutas.js
@@ -1,25 +1,17 @@
 const router=require('express').Router();
 const express=require('express');
 const middleware=require('./../Middleware/middleware');
-const clientControllers=require('./../controllers/ClientController');
+
 const adminControllers=require('./../controllers/AdminController');
-const serviceControllers=require('./../controllers/ServiceController');
+
 router.use(express.json());
 router.use(middleware);
 
-
-
 router.put('/actualizar_cliente',adminControllers.actualizarCliente);
 router.post('/agregar_cliente',adminControllers.agregarCliente);
-router.post('/agregar_servicio',serviceControllers.agregarservicio);
-router.get('/saldo',clientControllers.saldo);
 
-router.post('/cobrar',serviceControllers.cobrar);
 router.post('/consultarCliente',adminControllers.consultarClientePorId);
 router.put('/eliminarCliente',adminControllers.eliminarCliente);
 
-router.post('/eliminar_servicio',serviceControllers.eliminarServicio);
-router.get('/serviciosCons',clientControllers.serviciosCons);
-router.post('/transferir',clientControllers.transferir);
 
 module.exports=router;
